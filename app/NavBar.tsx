32b822d9b993ccf9cc95a04fb0ee337a85737edb
GdayRui <lirui.jobs@gmail.com>
2025-08-14 14:31:26 +1000

    Styled the active link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,14 @@
+"use client";
+
+import classnames from "classnames";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { FaBug } from "react-icons/fa";
 
 const NavBar = () => {
+  const currentPathname = usePathname();
+
   const links = [
     { label: "Dashboard", href: "/dashboard" },
     { label: "Tasks", href: "/tasks" },
@@ -17,7 +23,12 @@ const NavBar = () => {
           <li key={link.href}>
             <Link
               href={link.href}
-              className="text-gray-500 hover:text-gray-800 transition-colors duration-200 cursor-pointer"
+              className={classnames({
+                "text-gray-900": currentPathname === link.href,
+                "text-gray-500": currentPathname !== link.href,
+                "hover:text-gray-800 transition-colors duration-200 cursor-pointer":
+                  true,
+              })}
             >
               {link.label}
             </Link>
