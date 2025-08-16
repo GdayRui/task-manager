09e2710121fdf23e4314c22948736c707a522cde
GdayRui <lirui.jobs@gmail.com>
2025-08-16 17:53:53 +1000

    Setup Radix UI

---
+++ app/tasks/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "@radix-ui/themes";
+
+const TasksPage = () => {
+  return (
+    <div>
+      <h1>Tasks</h1>
+      <Button>Create Task</Button>
+    </div>
+  );
+};
+
+export default TasksPage;
